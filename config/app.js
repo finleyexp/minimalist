6619898a333d4c8fb1996b99f4da239c30aa83fb
Andy Bettisworth <andy@accreu.com>
2017-11-03 16:01:32 -0500

    add comments for app sections

--- config/app.js
+++ config/app.js
@@ -1,16 +1,38 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
 const express = require('express')
 const app = express()
 
-let port = 53003
+/**
+ * Globals
+ */
+
+const port = 53003
+
+/**
+ * Routes
+ */
 
 app.get("/", (req, res) => {
   res.status(200).send('Hello, friend!')
 })
 
-if (!module.parent) {
+/**
+ * Start server
+ */
+
+if (module === require.main) {
   app.listen(port, () => {
     console.log(`Running express.js app on port ${port}`)
   })
 }
+
+/**
+ * Export app
+ */
 
 module.exports = app
