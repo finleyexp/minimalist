3464487640416d93bb2b95df2db9a0f5344e5ce8
Andy Bettisworth <andy@accreu.com>
2017-11-03 20:09:38 -0500

    add constants and locals

--- config/app.js
+++ config/app.js
@@ -7,14 +7,40 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejs = require('ejs')
 
 /**
- * Globals
+ * Constants
  */
 
 const port = 53003
 const base = path.join(__dirname, '..')
 const env = process.env.NODE_ENV || 'development'
+const views = path.join(base, '/app/views')
+
+/**
+ * Locals
+ */
+
+app.locals.port = port
+app.locals.base = base
+app.locals.env = env
+app.locals.views = views
+
+/**
+ * Settings
+ */
+
+app.disable('x-powered-by')
+app.set('env', env)
+
+/**
+ * View engine
+ */
+
+app.engine('html.ejs', ejs.renderFile)
+app.set('view engine', '.html.ejs')
+app.set('views', views)
 
 /**
  * Routes
