80716b81199f669639858bb7dc2ec317f279f18b
Andy Bettisworth <andy@accreu.com>
2017-11-04 02:19:55 -0500

    git all migraitons

--- config/initializers/migrations.js
+++ config/initializers/migrations.js
@@ -1,10 +1,24 @@
 'use strict'
 
 const path = require('path')
+const fs = require('fs')
 
 module.exports = (app) => {
-  // check schema_migrations table for versions and match migrations
+  let migrations = []
+  let version
+
+  let files = fs.readdirSync(path.join(app.locals.base, '/db/migrations'))
+  for (let i=0; i < files.length; i++) {
+    if (/\d+/.test(files[i])) {
+      version = files[i].match(/\d+/)[0]
+      migrations.push({version: version, filename: files[i]})
+    }
+  }
+
+  app.locals.db.get
+  // check schema_migrations table for run versions
   // run any pending migrations using app.locals.db
   require(path.join(app.locals.base, '/db/migrations/20171104013050_create_table_containers'))(app)
+
   return true
 }
